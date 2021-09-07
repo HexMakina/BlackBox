fbc86cfc0d2262e1329c48c2b4884fe62f7d607f
sam <sammy@hexmakina.be>
2021-09-07 03:27:12 +0200

    added: RowInterface

---
+++ Database/RowInterface.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace HexMakina\Interfaces\Database;
+
+interface RowInterface
+{
+    public function table(): TableManipulationInterface;
+    
+    public function last_query();
+
+    public function last_alter_query(): QueryInterface;
+
+    public function is_new(): bool;
+
+    public function is_loaded(): bool;
+    public function is_altered(): bool;
+
+    public function export(): array;
+
+
+    /**
+     * loads row content from database,
+     *
+     * looks for primary key matching data in $dat_ass and sets the $load variable
+     * $load stays null if
+     * 1. not match is found in $dat_ass
+     * 2. multiple records are returned
+     * 3. no record is found
+     *
+     * @param  Array $dat_ass an associative array containing primary key data matches
+     * @return $this
+     */
+    public function load($dat_ass);
+
+
+    /**
+     * records changes vis-à-vis loaded data
+     *
+     * loops through the $dat_ass params
+     *
+     * @param  Array $dat_ass an associative array containing the new data
+     * @return $this
+     */
+    public function alter($dat_ass);
+
+    public function persist(): array;
+    public function wipe(): bool;
+
+    //------------------------------------------------------------  type:data validation
+    /**
+     * @return array containing all invalid data, indexed by field name, or empty if all valid
+     */
+    public function validate(): array;
+}
--- Database/TableManipulationInterface.php
+++ Database/TableManipulationInterface.php
@@ -2,20 +2,15 @@
 
 namespace HexMakina\Interfaces\Database;
 
-use HexMakina\Crudites\Queries\Insert;
-use HexMakina\Crudites\Queries\Update;
-use HexMakina\Crudites\Queries\Delete;
-use HexMakina\Crudites\Table\Row;
-
 interface TableManipulationInterface extends TableDescriptionInterface
 {
     // fetch or instanciate new Table\Rows
-    public function produce($dat_ass = []): Row;
-    public function restore($dat_ass): Row;
+    public function produce($dat_ass = []): RowInterface;
+    public function restore($dat_ass): RowInterface;
 
     // query generators
-    public function insert($values = []): Insert;
+    public function insert($values = []): QueryInterface;
     public function select($columns = null, $table_alias = null): SelectInterface;
-    public function update($modifications = [], $conditions = []): Update;
-    public function delete($conditions = []): Delete;
+    public function update($modifications = [], $conditions = []): QueryInterface;
+    public function delete($conditions = []): QueryInterface;
 }
