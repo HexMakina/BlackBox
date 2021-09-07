e8aca4f19d8db12e2630bc5ef797640211a321a8
sam <sammy@hexmakina.be>
2021-09-07 03:16:05 +0200

    improved: bindings handlers PSR-12 compliant

--- Database/QueryInterface.php
+++ Database/QueryInterface.php
@@ -5,16 +5,25 @@ namespace HexMakina\Interfaces\Database;
 interface QueryInterface
 {
     public function statement($setter = null): string;
-    public function bindings($setter = null);
 
     public function connection(ConnectionInterface $setter = null): ConnectionInterface;
 
-    public function table_name(): string;
+    public function tableName(): string;
     public function table(): TableManipulationInterface;
 
-    public function is_prepared();
-    public function is_executed($setter = null): bool;
-    public function is_success(): bool;
+    public function isPrepared();
+    public function isExecuted($setter = null): bool;
+    public function isSuccess(): bool;
+
+    public function setBindings($dat_ass);
+    public function getBindings(): array;
+
+    public function addBindings($assoc_data): array;
+    public function addBinding($field, $value, $table_name=null, $bind_label=null): string;
+
+    public function bindLabel($field, $table_name = null): string;
+
+    public function backTick($string, $table_name=null): string;
 
     public function run(): QueryInterface;
 }
