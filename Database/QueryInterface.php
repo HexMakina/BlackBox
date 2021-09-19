574e499290395a2f41508b56d73e9e378bd6ed21
sam <sammy@hexmakina.be>
2021-09-19 17:03:23 +0200

    added: getBindingNames() cleanup: commented binding methods

--- Database/QueryInterface.php
+++ Database/QueryInterface.php
@@ -8,23 +8,33 @@ interface QueryInterface
 
     public function connection(ConnectionInterface $setter = null): ConnectionInterface;
 
-    public function tableName(): string;
     public function table(): TableManipulationInterface;
+    public function tableName(): string;
+    public function tableAlias($setter = null): string;
 
     public function isPrepared();
     public function isExecuted($setter = null): bool;
     public function isSuccess(): bool;
 
     public function setBindings($dat_ass);
+
+    // returns assoc bind_label => bind_value
     public function getBindings(): array;
 
+    // return an assoc [column_name => bind_label]
+    public function getBindingNames(): array;
+
+    // returns an assoc of [field => binding name] of newly addded bindings
     public function addBindings($assoc_data): array;
+
+    // return the binding name computed or given for the field
     public function addBinding($field, $value, $table_name = null, $bind_label = null): string;
 
     public function bindLabel($field, $table_name = null): string;
 
     public function backTick($string, $table_name = null): string;
 
+    // public function join()
     public function joinedTables(): array;
 
     public function run(): QueryInterface;
