c96a5b2df524a7c6928f77891f7679c366787b71
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-11-13 22:28:15 +0100

    updated interfaces added types cleanup

--- Database/TraceInterface.php
+++ Database/TraceInterface.php
@@ -9,7 +9,14 @@ interface TraceInterface
     public const CODE_UPDATE = 'U';
     public const CODE_DELETE = 'D';
 
-    // public function queryCode($sql_statement): string;
+    public function queryCode() : string;
+    public function tableName(string $setter = null) : string;
+    public function tablePk(string $setter = null) : string;
+    public function operatorId(string $setter = null) : string;
 
-    // public function history($table_name, $table_pk, $sort='DESC') : array;
+
+    public function isUpdate(bool $setter = null) : bool;
+    public function isDelete(bool $setter = null) : bool;
+    public function isInsert(bool $setter = null) : bool;
+    public function isSelect(bool $setter = null) : bool;
 }
--- Database/TracerInterface.php
+++ Database/TracerInterface.php
@@ -4,17 +4,13 @@ namespace HexMakina\BlackBox\Database;
 
 interface TracerInterface
 {
-    public const CODE_CREATE = 'C';
-    public const CODE_SELECT = 'R';
-    public const CODE_UPDATE = 'U';
-    public const CODE_DELETE = 'D';
-
     public function tracingTable(): TableManipulationInterface;
 
     public function trace(TraceInterface $trace): bool;
-    public function traces($options = []): array;
 
-    // public function queryCode($sql_statement): string;
-
-    // public function history($table_name, $table_pk, $sort='DESC') : array;
+    /**
+     * @param array<string, mixed> $options
+     * @return array<array>
+     */
+    public function traces(array $options = []): array;
 }
