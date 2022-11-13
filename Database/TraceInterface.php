d7a436d862b75fb1a16c6728e260a61855ce97b1
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-11-13 22:29:31 +0100

    PSR12 and PHPSTAN 9 (almost)

--- Database/TraceInterface.php
+++ Database/TraceInterface.php
@@ -9,14 +9,14 @@ interface TraceInterface
     public const CODE_UPDATE = 'U';
     public const CODE_DELETE = 'D';
 
-    public function queryCode() : string;
-    public function tableName(string $setter = null) : string;
-    public function tablePk(string $setter = null) : string;
-    public function operatorId(string $setter = null) : string;
+    public function queryCode(): string;
+    public function tableName(string $setter = null): string;
+    public function tablePk(string $setter = null): string;
+    public function operatorId(string $setter = null): string;
 
 
-    public function isUpdate(bool $setter = null) : bool;
-    public function isDelete(bool $setter = null) : bool;
-    public function isInsert(bool $setter = null) : bool;
-    public function isSelect(bool $setter = null) : bool;
+    public function isUpdate(bool $setter = null): bool;
+    public function isDelete(bool $setter = null): bool;
+    public function isInsert(bool $setter = null): bool;
+    public function isSelect(bool $setter = null): bool;
 }
