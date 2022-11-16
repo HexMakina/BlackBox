a7490549c1c44c526ed77f523e2de41c715455f3
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-11-16 23:49:43 +0100

    - fixed return type for singleForeignKeyTo() - renamed ColumnInterface

--- Database/ColumnInterface.php
+++ Database/ColumnInterface.php
@@ -18,8 +18,9 @@ interface ColumnInterface
 
     public function default();
 
-    public function foreignTableName();
-
+    public function foreignTableName(): ?string;
+    public function foreignTableAlias(): ?string;
+    public function foreignColumnName(): ?string;
 
     public function setForeignTableName(?string $setter): void;
     public function setForeignColumnName(?string $setter): void;
--- Database/TableMetaInterface.php
+++ Database/TableMetaInterface.php
@@ -13,32 +13,32 @@ interface TableMetaInterface
     /** @return array<string,array> */
     public function describe($schema): array;
 
-    public function addColumn(TableColumnInterface $column);
+    public function addColumn(ColumnInterface $column);
     public function columns(): array;
-    public function column(string $name): ?TableColumnInterface;
+    public function column(string $name): ?ColumnInterface;
 
-    public function addPrimaryKey(TableColumnInterface $column);
+    public function addPrimaryKey(ColumnInterface $column);
     public function primaryKeys($with_values = null): array;
     public function primaryKeysMatch($dat_ass): array;
 
-    public function addForeignKey(TableColumnInterface $column);
+    public function addForeignKey(ColumnInterface $column);
 
-    /** @return array<string,TableColumnInterface> */
+    /** @return array<string,ColumnInterface> */
     public function foreignKeysByName(): array;
 
     /** @return array<string,array> */
     public function foreignKeysByTable(): array;
 
-    public function singleForeignKeyTo(TableMetaInterface $other_table): ?array;
+    public function singleForeignKeyTo(TableMetaInterface $other_table): ?ColumnInterface;
 
 
-    /** @param array<string,TableColumnInterface> $columns     */
+    /** @param array<string,ColumnInterface> $columns     */
     public function addUniqueKey(string $constraint_name, array $columns);
 
-    /** @return array<string,TableColumnInterface>    */
+    /** @return array<string,ColumnInterface>    */
     public function uniqueKeysByName(): array;
 
-    public function autoIncrementedPrimaryKey(TableColumnInterface $setter = null);
+    public function autoIncrementedPrimaryKey(ColumnInterface $setter = null);
 
     //EOF introspection
 }
