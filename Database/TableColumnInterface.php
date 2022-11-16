419cb792cbe8b2dc17e2ab8cf4b493d106d88572
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-11-16 23:27:07 +0100

    Fixed missing ; added setters and getters

--- Database/TableColumnInterface.php
+++ Database/TableColumnInterface.php
@@ -7,7 +7,7 @@ interface TableColumnInterface
     public function name(): string;
     public function tableName(): string;
 
-    public function type(): ?ColumnTypeInterface
+    public function type(): ?ColumnTypeInterface;
 
     public function isPrimary(): bool;
     public function isForeign($setter = null): bool;
@@ -19,4 +19,10 @@ interface TableColumnInterface
     public function default();
 
     public function foreignTableName();
+
+
+    public function setForeignTableName(?string $setter): void;
+    public function setForeignColumnName(?string $setter): void;
+    public function uniqueName($setter = null);
+    public function uniqueGroupName($setter = null);
 }
