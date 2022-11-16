e9fb42961f0ae23c6bbdaec69b67256d2d6f1e0c
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-11-16 23:27:41 +0100

    typing and cleanup

--- Database/ConnectionInterface.php
+++ Database/ConnectionInterface.php
@@ -4,22 +4,25 @@ namespace HexMakina\BlackBox\Database;
 
 interface ConnectionInterface
 {
-    public function __construct($dsn, $username = '', $password = '', $driver_options = []);
+    public function __construct(string $dsn, string $username = '', string $password = '', array $driver_options = []);
 
     public function driverName();
     public function databaseName(): string;
-    public function useDatabase($name);
+    public function useDatabase(string $name) : void;
+    public function restoreDatabase() : void;
+
+    public function errorCode(): ?string;
+    public function errorInfo(): array;
 
     public function prepare(string $sql_statement, $options = []) : ?\PDOStatement;
+    public function query(string $sql_statement, $fetch_mode = null, $fetch_col_num = null) : ?\PDOStatement;
+    public function alter(string $sql_statement) : ?int;
+
+    public function lastInsertId($name = null);
 
     public function transact(): bool;
     public function commit(): bool;
     public function rollback(): bool;
 
-    public function lastInsertId($name = null);
-    public function errorCode(): ?string;
-    public function errorInfo(): array;
 
-    public function query(string $sql_statement, ?int $fetch_mode = null, $fetch_col_num = null): ?\PDOStatement;
-    public function alter(string $sql_statement) : ?int;
 }
