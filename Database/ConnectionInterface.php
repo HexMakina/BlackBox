77655c319a489d0bb5c2f2eb47c09626f8a06ce1
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-11-16 14:08:33 +0100

    added typing

--- Database/ConnectionInterface.php
+++ Database/ConnectionInterface.php
@@ -10,7 +10,7 @@ interface ConnectionInterface
     public function databaseName(): string;
     public function useDatabase($name);
 
-    public function prepare($sql_statement, $options = []);
+    public function prepare(string $sql_statement, $options = []) : ?\PDOStatement;
 
     public function transact(): bool;
     public function commit(): bool;
@@ -20,6 +20,6 @@ interface ConnectionInterface
     public function errorCode(): ?string;
     public function errorInfo(): array;
 
-    public function query($sql_statement, $fetch_mode = null, $fetch_col_num = null);
-    public function alter($sql_statement);
+    public function query(string $sql_statement, ?int $fetch_mode = null, $fetch_col_num = null): ?\PDOStatement;
+    public function alter(string $sql_statement) : ?int;
 }
