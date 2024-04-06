55effec672cbf0e5a68b8cf0e522c66ac175cde0
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-04-06 17:04:42 +0200

    massive refactoring: start, detroy, set, has, get and cookies. that's it

--- StateAgentInterface.php
+++ StateAgentInterface.php
@@ -11,13 +11,20 @@ interface StateAgentInterface
 
     public static function getInstance(): StateAgentInterface;
 
+    public function start(): string;
+    public function destroy(): bool;
+
+    public function set(string $name, $value): void;
+    public function has(string $name): bool;
+    public function get(string $name);
+
     // camelCase wrapper for setcookie, coherent with getCookie
     public function setCookie(
         string $name,
-        string $value = '',
+        string $value = "",
         int $expires_in = 365 * 24 * 60 * 60,
-        string $path = '/',
-        string $domain = '',
+        string $path = "/",
+        string $domain = "",
         bool $secure = false,
         bool $httponly = false
     ): bool;
@@ -25,19 +32,6 @@ interface StateAgentInterface
     // returns the value stored or null
     public function getCookie(string $name): ?string;
 
-    public function addMessage(string $level, string $message, array $context = []): void;
-    public function messages(string $level = null);
-    public function resetMessages(string $level = null): void;
-
-    public function addRuntimeFilters(array $filters): void;
-    public function hasFilter(string $filter_name): bool;
-
-    public function addFilter(string $filter_name, string $value): void;
-    public function filters(string $filter_name = null, string $value = null): ?string;
-    public function resetFilters(string $filter_name = null): void;
-
     public function operatorId(string $setter = null): ?string;
     public function operatorCheckinDate(): string;
-
-    public function destroy(): bool;
 }
