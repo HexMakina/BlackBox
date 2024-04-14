6b69f1518522e94fb523642ee93c12bc72e9251f
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-04-14 21:06:34 +0200

    Remove addBindings method and add prepare method

--- Database/PreparedQueryInterface.php
+++ Database/PreparedQueryInterface.php
@@ -12,16 +12,15 @@ interface PreparedQueryInterface extends QueryInterface
     // return an assoc [column_name => bind_label]
     public function getBindingNames(): array;
 
-    // returns an assoc of [field => binding name] of newly addded bindings
-    public function addBindings($assoc_data): array;
-
     // return the binding name computed or given for the field
-    public function addBinding($field, $value, $table_name = null, $bind_label = null): string;
+    public function addBinding($field, $value, $table_name, $bind_label = null): string;
 
     public function bindLabel($field, $table_name = null): string;
 
     // public function join()
     public function joinedTables(): array;
+
+    public function prepare(): PreparedQueryInterface;
 
     public function run(): PreparedQueryInterface;
 }
