aa28853ed90a73cf7fad7e781a4d9bcfa60dbdad
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-12-14 22:14:51 +0100

    added doc

--- Database/ConnectionInterface.php
+++ Database/ConnectionInterface.php
@@ -6,11 +6,27 @@ interface ConnectionInterface
 {
     public function __construct(string $dsn, string $username = '', string $password = '', array $driver_options = []);
 
+    /**
+     * @return string The name of the database.
+     */
     public function database(): string;
-    public function schema(): SchemaInterface;
-    public function result($statement, $bindings = []): ResultInterface;
 
-    public function lastInsertId($name = null);
+    /**
+     * @return SchemaInterface The schema of the database.
+     */
+    public function schema(): SchemaInterface;
+    
+    /**
+     * @return The query builder.
+     */
+    public function crud();
+
+    /**
+     * @param mixed $statement The SQL statement to execute.
+     * @param array $bindings The values to bind to the statement.
+     * @return ResultInterface The result of the query.
+     */
+    public function result($statement, $bindings = []): ResultInterface;
 
     public function pdo(): \PDO;
 }
